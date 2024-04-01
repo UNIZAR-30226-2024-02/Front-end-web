0b009eab246f8971cc5e562dfcb528415dfb267c
815770 <815770@unizar.es>
2024-04-01 17:57:26 +0200

    Ranking
    El ranking está hecho entero, falta probarlo.

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RankingService } from './ranking.service';
 import { Ranking } from './ranking'
-
+import { NgFor } from '@angular/common';
+import { UsersService } from "../users/users.service";
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
@@ -9,6 +11,9 @@ import { Ranking } from './ranking'
 })
 export class RankingComponent {
   leaderboard: Ranking[] = [];
+  ngOnInit(): void{
+    this.getRanking();
+  }
   constructor(private rankingService: RankingService) { }
   getRanking(){
     this.rankingService.getRanking().subscribe(ranking => this.leaderboard = ranking);
--- src/app/ranking/ranking.service.ts
+++ src/app/ranking/ranking.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { MessageService } from "primeng/api";
 import { Observable, of } from "rxjs";
 import { Ranking } from './ranking'
+import { UsersService } from "../users/users.service";
+import { Router } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root'
-})
-export const R: Ranking[] = [
-  {idUsuario: "12", elo: "300"}
-];
+@Injectable({  providedIn: 'root'})
+
 
 export class RankingService {
-  constructor(private http: HttpClient, private messageService: MessageService) { }
+  constructor(private http: HttpClient, private usersService: UsersService, public router: Router) { }
+  private getHeaders(): HttpHeaders | null {
+    const token = this.usersService.getToken();
+    if (!token) {
+      // redirect the user to the login page if token does not exist
+      this.router.navigate(['/login']);
+      return null;
+    }
+
+    // setting up headers with authorization token
+    const headers = new HttpHeaders({
+      'Authorization': `${token}`
+    });
+
+    return headers;
+  }
   getRanking(): Observable<Ranking[]>{
-    return this.http.get<Ranking[]>("https://localhost:4000/ranking")
+    const headers = this.getHeaders();
+    if (!headers) {
+      return of([]);
+    }
+    return this.http.get<Ranking[]>("https://localhost:4000/ranking", { headers })
   }
 
 }
