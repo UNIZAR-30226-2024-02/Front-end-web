efec4ac12ac63329f8c71783132014975e594bea
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-04-13 15:09:31 +0200

    Pruebas mapa
    - Subido svg
    - Pulsaciones sobre cada pais funcionan

--- src/app/partida/partida.component.ts
+++ src/app/partida/partida.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-partida',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class PartidaComponent {
 
+  constructor(private toastr: ToastrService) { }
+  onRegionClick(regionId: string) {
+    console.log(`Se ha hecho clic en la región con ID: ${regionId}`);
+  }
+
+  onSVGLoad(event: any) {
+    const svgDoc = event.target.contentDocument;
+    if (svgDoc) {
+      // Busca todos los elementos <path> dentro del grupo con ID "map"
+      const paths = svgDoc.querySelectorAll('#map path');
+  
+      // Agrega un event listener a cada elemento <path>
+      paths.forEach((path: SVGElement) => {
+        path.addEventListener('click', (e: MouseEvent) => {
+          const targetId = path.id;
+          console.log(`Clic en la región con ID: ${targetId}`);
+          this.toastr.success(`Clicked on region with ID: ${targetId}`);
+        });
+      });
+    }
+  }
+  
+  
+  
+  
 }
--- src/app/partida/partida.service.ts
+++ src/app/partida/partida.service.ts
@@ -8,6 +8,7 @@ import { Observable } from "rxjs";
 export class PartidaService {
 
   constructor(private http: HttpClient) { }
+  //TODO ESTO ES FALSO NO FUNCIONA ASÍ :_:
   NuevaPartida(partida: any): Observable<any> {
     return this.http.post("http://localhost:4000/nuevaPartida", partida);
   }
