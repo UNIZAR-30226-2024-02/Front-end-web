fdb1097c265c4b6f779e6f430e3f09b67b2b7d53
Juanguppy <86721752+Juanguppy@users.noreply.github.com>
2024-04-14 15:12:49 +0200

    Modularización, colocación de tropas
    - Separada la funcionalidad en dos funciones, ya que si no es un chorizo
    - Ahora la colocación de tropas es dinámica
    - Se diferencian las tropas a poner por el jugador y su color (es stub)

--- src/app/partida/partida.component.ts
+++ src/app/partida/partida.component.ts
@@ -9,14 +9,27 @@ import { ToastrService } from 'ngx-toastr';
 export class PartidaComponent {
 
   numTropas = 0;
+  tropas: Map<string, { numTropas: number, user: string }>;
+  whoami : string = 'player1';
+  colorMap: Map<string, string>;
+  numJugadores: number = 3; // stub
+  etapa: string = 'preparacion'; // preparación, juego y final
+  subetapa: string = 'waiting'; // waiting, colocación, ataque, movimiento, carta
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService) {
+    this.tropas = new Map<string, { numTropas: number, user: string }>();
+    this.colorMap = new Map<string, string>();
+  }
+
   onRegionClick(regionId: string) {
-    console.log(`Se ha hecho clic en la región con ID: ${regionId}`);
+    console.log(`Se ha hecho clic en la región con ID: ${regionId}`)
   }
 
   ngOnInit() {
-    this.numTropas = 15;
+    this.numTropas = 35;
+    this.colorMap.set('player1', 'verde');
+    this.colorMap.set('player2', 'rojo');
+    this.colorMap.set('player3', 'azul');
   }
 
   onSVGLoad(event: any) {
@@ -33,89 +46,125 @@ export class PartidaComponent {
           const imgWidth = 50;
           const imgHeight = 50;
 
-          // Ask the user for the number of troops
-          const troops = window.prompt('How many troops do you want to add?');
-
-          // Check if the user clicked the Cancel button
-          if (troops === null) {
-            return;
-          }
-
-          const numTroops = parseInt(troops);
-
-          if (numTroops > this.numTropas) {
-            this.toastr.error('¡No tienes suficientes tropas!');
-            return;
-          }
-      
-          this.numTropas -= numTroops;
-
-          // Check if the input is a valid number
-          if (isNaN(numTroops) || numTroops < 1) {
-            alert('Please enter a valid number of troops.');
-            return;
-          }
-
-          // Check if the input is a valid number
-          if (isNaN(numTroops) || numTroops < 1) {
-            alert('Please enter a valid number of troops.');
-            return;
-          }
-
-          // Get the point at which the click event occurred
-          const pt = svgDoc.documentElement.createSVGPoint();
-          pt.x = e.clientX;
-          pt.y = e.clientY;
-
-          // Transform the point to the SVG coordinate system
-          const svgP = pt.matrixTransform(svgDoc.documentElement.getScreenCTM().inverse());
-
-          const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-          text.setAttribute('x', svgP.x.toString());
-          text.setAttribute('y', (svgP.y + imgHeight / 2).toString());
-          text.setAttribute('text-anchor', 'middle');
-          text.setAttribute('font-size', '20');
-          text.setAttribute('fill', 'black'); 
-          text.setAttribute('stroke', 'white'); 
-          text.setAttribute('stroke-width', '1');
-          text.textContent = numTroops.toString();
-
-          // Calculate the number of each type of image to add
-          const numTanks = Math.floor(numTroops / 10);
-          let remainingTroops = numTroops % 10;
-          const numHorses = Math.floor(remainingTroops / 5);
-          remainingTroops %= 5;
-
-          // Function to create and append an image
-          const addImage = (href: string, index: number) => {
-            const img = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-            img.setAttributeNS('http://www.w3.org/1999/xlink', 'href', href);
-            img.setAttribute('width', imgWidth.toString());
-            img.setAttribute('height', imgHeight.toString());
-            img.setAttribute('x', (svgP.x - imgWidth / 2 + index * imgWidth * 0.15).toString());
-            img.setAttribute('y', (svgP.y - imgHeight / 2).toString());
-            svgDoc.documentElement.appendChild(img);
-          };
-
-          // Add the images
-          let index = 0;
-          for (let i = 0; i < numTanks; i++, index++) {
-            addImage('/assets/tanque_verde.png', index);
-          }
-          for (let i = 0; i < numHorses; i++, index++) {
-            addImage('/assets/caballo_verde.png', index);
-          }
-          for (let i = 0; i < remainingTroops; i++, index++) {
-            addImage('/assets/infanteria_verde.png', index);
-          }
-          svgDoc.documentElement.appendChild(text);
+          this.colocarTropas(e, svgDoc, imgWidth, imgHeight, this.whoami);
         });
-        
+
+        if (!this.tropas.has(path.id)) {
+          this.tropas.set(path.id, { numTropas: 0, user: '' });
+        }
+
       });
     }
   }
-  
-  
-  
+
+  colocarTropas(e: MouseEvent, svgDoc: Document, imgWidth: number, imgHeight: number, user: string) {
+    // Ask the user for the number of troops
+    const troops = window.prompt('How many troops do you want to add?');
+    const terrainId = (e.target as SVGElement).id;
+    console.log(this.tropas)
+    console.log(this.whoami)
+
+    // Check if the user clicked the Cancel button
+    if (troops === null) {
+      return;
+    }
+
+    let numTroops = parseInt(troops);
+
+    if (numTroops > this.numTropas) {
+      this.toastr.error('¡No tienes suficientes tropas!');
+      return;
+    }
+
+    this.numTropas -= numTroops;
+
+    const terrainInfo = this.tropas.get(terrainId);
+    if (terrainInfo) {
+      terrainInfo.numTropas += numTroops;
+      terrainInfo.user = user;
+      numTroops = terrainInfo.numTropas;
+    } else {
+      this.tropas.set(terrainId, { numTropas: numTroops, user });
+    }
+
+    // Check if the input is a valid number
+    if (isNaN(numTroops) || numTroops < 1) {
+      alert('Please enter a valid number of troops.');
+      return;
+    }
+
+    // Get the point at which the click event occurred
+    const svgElement = svgDoc.rootElement as SVGSVGElement;
+    if (!svgElement) {
+      console.error('SVG element not found');
+      return;
+    }
+
+    const pt = svgElement.createSVGPoint();
+    pt.x = e.clientX;
+    pt.y = e.clientY;
+
+    // Transform the point to the SVG coordinate system
+    const screenCTM = svgElement.getScreenCTM();
+    if (!screenCTM) {
+      console.error('Unable to get screen CTM');
+      return;
+    }
+
+    const svgP = pt.matrixTransform(screenCTM.inverse());
+
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    text.setAttribute('x', svgP.x.toString());
+    text.setAttribute('y', (svgP.y + imgHeight / 2).toString());
+    text.setAttribute('text-anchor', 'middle');
+    text.setAttribute('font-size', '20');
+    text.setAttribute('fill', 'black'); 
+    text.setAttribute('stroke', 'white'); 
+    text.setAttribute('stroke-width', '1');
+    text.setAttribute('data-terrain-id', terrainId);
+    text.textContent = numTroops.toString();
+
+    // Calculate the number of each type of image to add
+    const numTanks = Math.floor(numTroops / 10);
+    let remainingTroops = numTroops % 10;
+    const numHorses = Math.floor(remainingTroops / 5);
+    remainingTroops %= 5;
+
+    // Function to create and append an image
+    const addImage = (href: string, index: number) => {
+      const img = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+      img.setAttributeNS('http://www.w3.org/1999/xlink', 'href', href);
+      img.setAttribute('width', imgWidth.toString());
+      img.setAttribute('height', imgHeight.toString());
+      img.setAttribute('x', (svgP.x - imgWidth / 2 + index * imgWidth * 0.15).toString());
+      img.setAttribute('y', (svgP.y - imgHeight / 2).toString());
+      img.setAttribute('data-terrain-id', terrainId);
+      svgDoc.documentElement.appendChild(img);
+    };
+
+    // Delete every image in the region clicked
+    // Delete every image and text in the region clicked
+    const elements = svgDoc.querySelectorAll(`image[data-terrain-id="${terrainId}"], text[data-terrain-id="${terrainId}"]`);
+    elements.forEach(element => {
+      if (element.parentNode) {
+        element.parentNode.removeChild(element);
+      }
+    });
+
+    // Add the images
+    let index = 0;
+    console.log(this.whoami)
+    console.log(this.colorMap.get(this.whoami))
+    for (let i = 0; i < numTanks; i++, index++) {
+      addImage(`/assets/tanque_${this.colorMap.get(this.whoami)}.png`, index);
+    }
+    for (let i = 0; i < numHorses; i++, index++) {
+      addImage(`/assets/caballo_${this.colorMap.get(this.whoami)}.png`, index);
+    }
+    for (let i = 0; i < remainingTroops; i++, index++) {
+      addImage(`/assets/infanteria_${this.colorMap.get(this.whoami)}.png`, index);
+    }
+    svgDoc.documentElement.appendChild(text);
+  }
   
 }
