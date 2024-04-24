649a748e9c58c0b5240b3244849905233de182d3
815770 <815770@unizar.es>
2024-04-24 02:20:51 +0200

    Historial hecho
    He sustituido la corona por el emoticono de la estrella de UTF-8

--- src/app/historial/historial.component.ts
+++ src/app/historial/historial.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Partida } from '../partidas/partidas.component';
 import { HistorialService  } from './historial.service';
+import { NgFor } from '@angular/common';
 
 
 @Component({
@@ -22,8 +23,22 @@ export class HistorialComponent {
     this.historialService.getHistorial().subscribe(listaPartidas => this.partidas = listaPartidas);
   }
 
-  esGanador(jugador : string, id : string){
+  esGanador(jugador : string, id : string):boolean{
     let foundPartida = this.partidas.find(partida => partida._id === id);
     return foundPartida ? jugador === foundPartida.ganador : false;
+  }
+  getJugadores(p :Partida):string{
+    var retVal:string = ""
+    for (let i = 0; i < p.jugadores.length; i++) {
+      retVal += p.jugadores[i].usuario;
+      if (this.esGanador(p.jugadores[i].usuario,p._id)) {
+        retVal+="&#11088;";
+      }
+      if (i<p.jugadores.length-1) {
+        retVal+=",";
+      }
+      
+    }
+    return retVal;
   }
 }
