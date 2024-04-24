d9f44865a4ca5f96f44bb41b51beedc15839f4bd
815770 <815770@unizar.es>
2024-04-24 16:51:35 +0200

    Control del formuario de registro
    Control para que la contraseña sea segura y el correo válido. El correo lo controla con regex, por lo que existe la posibilidad de introducir un correo inexistente. Además he cambiado el css del login para que sea como el de registro.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { UsersService } from "../users/users.service";
 import { Router } from "@angular/router";
 import { Socket } from 'ngx-socket-io';
 import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -21,18 +22,39 @@ export class RegisterComponent {
   }
 
   register() {
-    const user = { idUsuario: this.idUsuario, password: this.password, correo: this.correo };
-    this.userService.register(user).subscribe(
-      (data) => {
-        this.userService.setToken(data.token);
-        this.userService.setUsername(data.idUsuario);
-        this.socket.emit('login', this.idUsuario);
-        this.router.navigateByUrl("/menu");
-      },
-      (error) => {
-        this.toastr.error(error.error.error, 'Error', { timeOut: 5000 });
+    if (this.esCorreo()) {
+      if (this.esSegura()) {
+        const user = { idUsuario: this.idUsuario, password: this.password, correo: this.correo };
+        this.userService.register(user).subscribe(
+          (data) => {
+            this.userService.setToken(data.token);
+            this.userService.setUsername(data.idUsuario);
+            this.socket.emit('login', this.idUsuario);
+            this.router.navigateByUrl("/menu");
+          },
+          (error) => {
+            this.toastr.error(error.error.error, 'Error', { timeOut: 5000 });
+          }
+        );
       }
-    );
+      else{
+        console.error('Contraseña no Válido');
+        this.toastr.error("La contraseña debe contener:</br>- 8 o más caracteres</br>- Al menos una letra minúscula</br>- Al menos una letra mayúscula</br>- Al menos un número", '', { closeButton: true, timeOut: 4000, progressBar: true, enableHtml: true });
+      }
+    }
+    else{
+      console.error('Correo no Válido');
+      this.toastr.error(this.correo +" no es un correo válido.");
+    }
   }
 
+  esSegura():boolean{
+    var regExpPasswd = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/);
+    return regExpPasswd.test(this.password);
+  }
+
+  esCorreo():boolean{
+    var regExpMail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return regExpMail.test(this.correo);
+  }
 }
